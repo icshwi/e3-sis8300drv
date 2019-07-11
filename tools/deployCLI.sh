b1a278b503d71fe4eaef06e29c4f63afbfcb2561
thomasfay <thomas.fay@esss.se>
2019-07-11 09:30:05 +0200

    Corrects error when checking for existing linked tools.

--- tools/deployCLI.sh
+++ tools/deployCLI.sh
@@ -17,31 +17,29 @@ sourcePath=$EPICS_SRC/e3-sis8300drv/m-kmod-sis8300/src/main/c
 
 tools_version=$(grep -e E3_MODULE_VERSION "$configPath/CONFIG_MODULE")
 tools_version=${tools_version#'E3_MODULE_VERSION:='}
-echo "tools version = $tools_version"
 
 # Deploy tools from local build directory to installation path
 
 #Remove any existing configuration
+echo "Checking $installPath for existing sis8300 tools..."
 for i in `ls $sourcePath | grep $tools_version`
 do
     tool_name=${i%"_$tools_version"}
-    if [ -L $instalPath/$tool_name ]
-    then
-        unlink "$installPath/$tool_name"
+    if [ -L $installPath/$tool_name ];then
+        echo "Removing existing $tool_name entry"
+        eval "unlink $installPath/$tool_name"
     fi
 done
 
 # Link the latest version of the sis830d0rv command line tools
+echo "Linking latest version of tools into $installPath..."
 for i in `ls $sourcePath | grep $tools_version`
 do
     tool_name=${i%"_$tools_version"}
     if [ -L $installPath/$tool_name ]; then
-        echo "$installPath/$tool_name"
         echo "$tool_name already exists in $installPath"
     else
-        echo "1"
-        echo "$installPath"
-        echo "$tool_name"
-	    eval "ln -s $sourcePath/$i $installPath/$tool_name"
+	echo "Linking version $tools_version of $tool_name"
+	eval "ln -s $sourcePath/$i $installPath/$tool_name"
     fi
 done
